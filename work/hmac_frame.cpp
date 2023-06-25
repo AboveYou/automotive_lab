ec3f63f16ac0b89dfc5911f3a5faffefd5571d57
Sebastian Schirle <60340491+AboveYou@users.noreply.github.com>
2023-06-25 14:56:22 +0200

    unite hmac_frame
    - only one main with parameter
    - edit makefile

--- work/hmac_frame.cpp
+++ work/hmac_frame.cpp
@@ -5,6 +5,7 @@
 #include <cryptopp/sha.h>
 #include <cryptopp/hex.h>
 
+#include "canfd_send.h"
 #include "canfd_recv.h"
 
 using namespace CryptoPP;
@@ -32,7 +33,7 @@ void verify_frame(CryptoPP::byte *key, const canfd_frame& frame) {
     memcpy(message, &frame.data, message_len);
 
     CryptoPP::byte receivedDigest[HMAC<SHA256>::DIGESTSIZE];
-    memcpy(receivedDigest, frame.data + message_len, SHA256::DIGESTSIZE);
+    memcpy(receivedDigest, frame.data + message_len, HMAC<SHA256>::DIGESTSIZE);
 
     // Verify the received message by hashing it and comparing the hash with the received hash
     CryptoPP::byte calculatedDigest[HMAC<SHA256>::DIGESTSIZE];
@@ -50,20 +51,61 @@ void verify_frame(CryptoPP::byte *key, const canfd_frame& frame) {
     }
 }
 
-int main() {
+void print_usage() {
+    cout << "usage:" << endl << "\t-s = send" << endl << "\t-r = recv" << endl;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        print_usage();
+        return 1;
+    }
+
     CryptoPP::byte key[] = {
-        0x01, 0x02, 0x03, 0x04,
-        0x01, 0x02, 0x03, 0x04,
-        0x01, 0x02, 0x03, 0x04,
+            0x01, 0x02, 0x03, 0x04,
+            0x01, 0x02, 0x03, 0x04,
+            0x01, 0x02, 0x03, 0x04,
     };
 
-    canfd_frame frame;
-    memset(&frame, 0, sizeof(frame));
+    if (strcmp(argv[1], "-s") == 0) {
+        cout << "acting as sender" << endl;
+        CryptoPP::byte message[] = {
+                0x68, 0x61, 0x63, 0x6b, 0x20,
+                0x74, 0x68, 0x65, 0x20, 0x70,
+                0x6c, 0x61, 0x6e, 0x65, 0x74
+        };
 
-    CANFDReceiver receiver("vcan0");
-    
-    receiver.receiveFrame(frame);
-    verify_frame(key, frame);
-    
+        CryptoPP::byte digest[HMAC<SHA256>::DIGESTSIZE];
+
+        hmac_payload(key, message, digest);
+
+        canfd_frame frame;
+        memset(&frame, 0, sizeof(frame));
+        frame.can_id = 0x7334;
+        frame.len = sizeof(message) + HMAC<SHA256>::DIGESTSIZE;
+        memcpy(frame.data, message, sizeof(message));
+        memcpy(frame.data + sizeof(message), digest, HMAC<SHA256>::DIGESTSIZE);
+
+        CANFDSender sender("vcan0");
+
+        sender.sendFrame(frame);
+        sender.printFrame(frame);
+    }
+    else if (strcmp(argv[1], "-r") == 0) {
+        cout << "acting as receiver" << endl;
+        canfd_frame frame;
+        memset(&frame, 0, sizeof(frame));
+
+        CANFDReceiver receiver("vcan0");
+
+        receiver.receiveFrame(frame);
+        verify_frame(key, frame);
+    }
+    else {
+        cout << "the parameter is invalid!" << endl;
+        print_usage();
+        return 1;
+    }
+
     return 0;
 }
